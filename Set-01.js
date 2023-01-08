51f3773fdccd263848b6abb5bac64ebdb7ca0775
LCH6890 <lorenachamorroalameda@gmail.com>
2023-01-08 10:03:05 +0100

    Se añade ejercicio 1

---
+++ Set-01.js
@@ -0,0 +1,11 @@
+const familia = ["Pepe", "Alicia", "Lorena"]
+
+const miSet = new Set(familia)
+console.log(familia)
+console.log(miSet)
+
+miSet.add("Lorena")
+console.log(miSet)
+
+miSet.add("Javascript")
+console.log(miSet)
